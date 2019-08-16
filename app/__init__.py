7e3bb98729e9f0dcf29120d88907931364490a1a
Tegar Imansyah <tegar@alterra.id>
2019-08-16 13:24:15 +0700

    add list of surah

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,24 @@
 from flask import Flask
+
+from app.surah import list_of_surah
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    data = ""
+    for index, surah in list_of_surah.items():
+        data += f'<a href="/surah/{index}">{index} - {surah.get("ar")} ({surah.get("id")})</a><br>'
+    return data
+
+@app.route("/surah/<int:id>")
+def display_surah(id):
+    try:
+        assert id >= 1 and id <= 114, "Not Found"
+        surah = list_of_surah.get(id)
+        return f"Surah {surah.get('ar')} - {surah.get('id')}"
+    except Exception as e:
+        return f"{e}"
 
 if __name__ == "__main__":
     app.run()
