c17dbbf257fc14c64d4ad9ed732c1b5675747eb6
Tegar Imansyah <tegar@alterra.id>
2019-10-12 22:22:43 +0700

    create loading on fetch

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import Logo from '../../assets/images/logo.svg';
-
 class HomeScreen extends React.Component {
     render() {
         return (
             <div className="container relative flex content-between flex-wrap h-screen lg:w-8/12 sm:w-10/12 md:w-10/12">
                     <h3>Home of Quran Affairs</h3>
-                    <div className="character-place">
-                        <img className="character" src={Logo} alt="React Logo" />
-                    </div>
                     <Link to="/reading_list">Go to somewhere else</Link>
             </div>
         );
--- src/components/QuranList/quranlist.js
+++ src/components/QuranList/quranlist.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { trackPromise } from 'react-promise-tracker';
+import LoadingIndicator from '../LoadingIndicator/loading'
+import Axios from 'axios';
 
 class ReadingList extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            surah: [],
+        };
+
+        this.LoadData = this.LoadData.bind(this);
+    }
+
+    LoadData() {
+        this.setState({
+            surah: [],      
+        });
+
+        trackPromise(
+            Axios.get('http://api.alquran.cloud/v1/surah/1').then((response) => {
+                console.log(response);
+              }, (error) => {
+                console.log(error);
+              })
+        );
+    }
+
     render() {
         return (
             <div className="container relative flex content-between flex-wrap h-screen lg:w-8/12 sm:w-10/12 md:w-10/12">
                     <h3>Home of Quran Affairs - Reading List</h3>
-                    <Link to="/">Go to home</Link>
+                    <Link to="/">Go to home</Link><br />
+                    <button onClick={this.LoadData} title="Click me">Click Me</button>
+                    <LoadingIndicator/>
             </div>
         );
     }
