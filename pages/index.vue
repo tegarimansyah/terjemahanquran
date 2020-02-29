b625b1dcd37cded41f1a6ac37686376bcca4dc9e
tegarimansyah <egar.imansyah@gmail.com>
2020-02-29 21:53:32 +0700

    add clear statement if find mistake

--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,14 @@
     <h1 class="text-2xl font-bold">
       Enjoy The Qur'an
     </h1>
-    <em class="text-sm">Alpha version, need more collaborator for this <a href="https://github.com/tegarimansyah/quran.urusan.id" class="text-blue-600">open source project</a></em>
-    <p>Read Al Qur'an or translation. Understanding what you read. This site still a Proof of Concept.</p>
+    <p>Read Al Qur'an or translation. Understanding what you read.</p>
+    <br>
+    <em class="text-sm block">Alpha version, need more collaborator for this <a href="https://github.com/tegarimansyah/quran.urusan.id" class="text-blue-600">open source project</a>. <strong>This site still a Proof of Concept.</strong></em>
+    <em class="text-sm block">If you find any mistake, please <a href="https://github.com/tegarimansyah/quran.urusan.id/issues/new" class="text-blue-600">leave a comment</a> or email me: <a href="mailto:imansyahtegar@gmail.com" class="text-blue-600">imansyahtegar [at] gmail [dot] com</a></em>
     <hr class="my-3">
     <p><strong>Things to add</strong>: Bookmark, separate by theme, full offline capability, jump to section, add our recitation.</p>
     <hr class="my-3">
-    <div class="grid sm:grid-cols-2 lg:grid-cols-4 gap-4">
+    <div class="grid sm:grid-cols-2 lg:grid-cols-4 gap-4 text-center">
       <nuxt-link :id="index+1" v-for="(surah, index) in surah_list" :key="index+1" :to="`/${index+1}`">
         <div class="mt-1 mb-3 p-1 pt-3 col-span-1 hover:bg-gray-300">
           <span class="block font-semibold">{{ index+1 }} - {{ surah.id.surah_name }}</span>
