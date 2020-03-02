12f40b3523eb808920caaedb4ebe19eb39ffcd7f
tegarimansyah <egar.imansyah@gmail.com>
2020-03-02 13:01:12 +0700

    add sub themes for surah

--- pages/_surah/load_data.vue
+++ pages/_surah/load_data.vue
@@ -1,10 +1,22 @@
 <template>
   <div class="p-10">
     <h1>Loading content</h1>
+    <ul>
+      <li v-for="(theme, index) in theme_list.theme" :key="index+1">
+        {{ theme.from }} - {{ theme.to }}: {{ theme.text }}
+      </li>
+    </ul>
   </div>
 </template>
 <script>
+import themeList from '~/components/theme.js'
+
 export default {
+  data () {
+    return {
+      theme_list: themeList(this.$route.params.surah)
+    }
+  },
   created () {
     this.surah_number = this.$route.params.surah
     this.$axios.$get(`${localStorage.base_url}/surah/${this.surah_number}/editions/${localStorage.edition},simple-quran`)
