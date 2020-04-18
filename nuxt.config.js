65ae18e1f12364a0952d81095bd2051b181b5945
tegarimansyah <egar.imansyah@gmail.com>
2020-04-18 20:18:55 +0700

    Fixing Default Meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Al Qur\'an Interaktif' || process.env.npm_package_name,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
