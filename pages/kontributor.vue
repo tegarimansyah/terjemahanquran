d39056870e15e051d322575214fd5e73be470ab3
Tegar Imansyah <tegarimansyah@users.noreply.github.com>
2020-04-26 16:24:42 +0700

    add credit and contribution

--- pages/kontributor.vue
+++ pages/kontributor.vue
@@ -1,5 +1,18 @@
 <template>
+<div>
   <ComingSoon />
+  Bismillah hirrahman nirrahim
+  
+  Syukur Alhamdulillah, kami dapat mempersembahkan website untuk Umat Islam Indonesia. Tujuan dari website ini adalah membantu masyarakat untuk belajar membaca, mempelajari arti serta tafsir, merekam bacaan untuk didengar setiap waktu atau dibagikan kepada guru untuk diperbaiki.
+  
+  <p>Apakah belajar Al Qur'an di website ini adalah metode terbaik? Tidak juga, metode terbaik tetap bertemu langsung dengan guru dan belajar dari beliau. Namun jika hal itu tidak bisa dilakukan karena terpisah jarak atau ingin berlatih sendiri, maka website ini dapat menjadi alternatifnya.</p>
+  
+  Dalam membuat ini, kami menggunakan berbagai sumber daya sebagai berikut
+  * Al Qur'an bahasa arab, translasi bahasa indonesia serta tafsir dari web dan aplikasi mobile Al Qur an kemenag
+  * Topik dan sub topik dari Al Qur an MUI
+  
+</div>
+
 </template>
 
 <script>
